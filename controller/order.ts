b9b9ec879ca98f23cab96cd8d4b6f6e353cdeecc
Abdelhadi El aoudi <ab.elaoudi@gmail.com>
2023-11-13 08:49:56 +1300

    fix some codes

--- controller/order.ts
+++ controller/order.ts
@@ -3,36 +3,45 @@ import { Order } from "../db/schemas";
 
 // Insert a new category
 const insertOne = async (req: Request, res: Response) => {
-    const item = new Order(req.body)
-    await item.save()
-    res.send(item)
-}
+  const item = new Order(req.body);
+  await item.save();
+  res.send(item);
+};
 
 // Find all categories
 const findAll = async (req: Request, res: Response) => {
-    const items = await Order.find()
-    res.send(items)
-}
+  const items = await Order.find();
+  res.send(items);
+};
 
 // Finds a single category by its _id
 const findById = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const item = await Order.findById(id).populate('order_lines')
-    res.send(item)
-}
+  const { id } = req.params;
+  const item = await Order.findById(id).populate({
+    path: "order_lines",
+    populate: { path: "product", select: "_id" },
+  });
+  res.send(item);
+};
 
 // Finds and delete a single document by its _id
 const findByIdAndUpdate = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const item = await Order.findByIdAndUpdate(id, req.body, { new: true })
-    res.send(item)
-}
+  const { id } = req.params;
+  const item = await Order.findByIdAndUpdate(id, req.body, { new: true });
+  res.send(item);
+};
 
 // Finds and update a single document by its _id
 const findByIdAndDelete = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const cat = await Order.findByIdAndDelete(id)
-    res.send(cat)
-}
+  const { id } = req.params;
+  const cat = await Order.findByIdAndDelete(id);
+  res.send(cat);
+};
 
-export default { insertOne, findAll, findById, findByIdAndUpdate, findByIdAndDelete }
+export default {
+  insertOne,
+  findAll,
+  findById,
+  findByIdAndUpdate,
+  findByIdAndDelete,
+};
--- db/schemas.ts
+++ db/schemas.ts
@@ -7,83 +7,88 @@ import { Schema, model, Types } from "mongoose";
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   desc: { type: String },
-  parent_id: { type: Types.ObjectId, ref: 'Category' }, 
+  parent_id: { type: Types.ObjectId, ref: "Category" },
 });
 
 // 3. Create a Model.
-const Category = model('Category', CategorySchema);
+const Category = model("Category", CategorySchema);
 
 /* 
 // Product.
 */
 // 2. Create a Schema corresponding to the document interface.
-const ProductSchema = new Schema({
-    category_id: { type: Types.ObjectId, ref: 'Category', required: true },
+const ProductSchema = new Schema(
+  {
+    category_id: { type: Types.ObjectId, ref: "Category", required: true },
     name: { type: String, required: true },
     desc: { type: String, required: true },
     SKU: { type: String },
     price: { type: Number, required: true },
-    image: { type: String, required: true},
-    published: { type: Boolean, default: false }
-}, { timestamps: true });
+    image: { type: String, required: true },
+    published: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 // 3. Create a Model.
-const Product = model('Product', ProductSchema);
+const Product = model("Product", ProductSchema);
 
 /* 
 // Inventory.
 */
 // 2. Create a Schema corresponding to the document interface.
-const InventorySchema = new Schema({
-    product_id: { type: Types.ObjectId, ref: 'Product', required: true },
+const InventorySchema = new Schema(
+  {
+    product_id: { type: Types.ObjectId, ref: "Product", required: true },
     quantity_in_stock: { type: Number, default: 0 },
     sales: { type: Number, default: 0 },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 // 3. Create a Model.
-const Inventory = model('Inventory', InventorySchema);
+const Inventory = model("Inventory", InventorySchema);
 
 /* 
 // Order.
 */
 // 2. Create a Schema corresponding to the document interface.
-const OrderSchema = new Schema({
-//   customer_id: { type: Types.ObjectId, ref: 'User' },
-  status: { type: String, enum: ['created', 'paid']},
-  net_amount: { type: Number, required: true },
-  shipped_date: { type: Date, ref: 'Order' }, 
-  shipping_charges: { type: Number},
-  shipping_adress: { 
-    number: {type: String, required: true},
-    street: {type: String, required: true},
-    city: {type: String, required: true},
-    postal_code: {type: String, required: true}
-   },
-  tax: { type: Number, default: 0 },
-  total_amount: { type: Number, required: true },
-  order_lines: [
-    { type: Types.ObjectId, ref: 'OrderLines'}
-  ]
-}, { timestamps: true });
+const OrderSchema = new Schema(
+  {
+    //   customer_id: { type: Types.ObjectId, ref: 'User' },
+    status: { type: String, enum: ["created", "paid"] },
+    net_amount: { type: Number, required: true },
+    shipped_date: { type: Date, ref: "Order" },
+    shipping_charges: { type: Number },
+    shipping_adress: {
+      number: { type: String, required: true },
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      postal_code: { type: String, required: true },
+    },
+    tax: { type: Number, default: 0 },
+    total_amount: { type: Number, required: true },
+    order_lines: [{ type: Types.ObjectId, ref: "OrderLines" }],
+  },
+  { timestamps: true }
+);
 
 // 3. Create a Model.
-const Order = model('Order', OrderSchema);
+const Order = model("Order", OrderSchema);
 
 /* 
 // Order items.
 */
 // 2. Create a Schema corresponding to the document interface.
 const OrderLinesSchema = new Schema({
-  order_id: { type: Types.ObjectId, ref: 'Order' },
-//   product_id: { type: Types.ObjectId, ref: 'Product' },
+  product: { type: Types.ObjectId, ref: "Product" },
   quantity: { type: Number, required: true },
   list_price: { type: Number, required: true },
   discount: { type: Number },
 });
 
 // 3. Create a Model.
-const OrderLines = model('OrderLines', OrderLinesSchema);
-
+const OrderLines = model("OrderLines", OrderLinesSchema);
 
 // Export all models
-export { Product, Category, Order, OrderLines }
+export { Product, Category, Order, OrderLines };
