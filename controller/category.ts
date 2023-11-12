7696d4e18079a9f0b38ee95d1fbc685ccfd9ef41
Abdelhadi El aoudi <ab.elaoudi@gmail.com>
2023-11-13 10:34:29 +1300

    Category controller refactory

--- controller/category.ts
+++ controller/category.ts
@@ -3,36 +3,48 @@ import { Category } from "../db/schemas";
 
 // Insert a new category
 const insertOne = async (req: Request, res: Response) => {
-    const cat = new Category(req.body)
-    await cat.save()
-    res.send(cat)
-}
+  const cat = new Category(req.body);
+  await cat.save();
+  res.send(cat);
+};
 
 // Find all categories
 const findAll = async (req: Request, res: Response) => {
-    const cats = await Category.find()
-    res.send(cats)
-}
+  const cats = await Category.find();
+  res.send(cats);
+};
 
 // Finds a single category by its _id
 const findById = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const cat = await Category.findById(id)
-    res.send(cat)
-}
+  const { id } = req.params;
+  const cat = await Category.findById(id);
+  res.send(cat);
+};
 
 // Finds and delete a single document by its _id
 const findByIdAndUpdate = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const cat = await Category.findByIdAndUpdate(id, req.body, { new: true })
-    res.send(cat)
-}
+  const { id } = req.params;
+  try {
+    const cat = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.send(cat);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Finds and update a single document by its _id
 const findByIdAndDelete = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const cat = await Category.findByIdAndDelete(id)
-    res.send(cat)
-}
+  const { id } = req.params;
+  const cat = await Category.findByIdAndDelete(id);
+  res.send(cat);
+};
 
-export default { insertOne, findAll, findById, findByIdAndUpdate, findByIdAndDelete }
+export default {
+  insertOne,
+  findAll,
+  findById,
+  findByIdAndUpdate,
+  findByIdAndDelete,
+};
